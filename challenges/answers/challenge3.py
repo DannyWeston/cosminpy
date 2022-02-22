e8b996acd33aecf0b9c326efdd4b8a42f290b087
Admin <danielweston37@gmail.com>
2022-02-22 13:48:21 +0000

    Complete solution

---
+++ challenges/answers/challenge3.py
@@ -0,0 +1,38 @@
+# Challenge 3
+#
+#   Description:
+#   
+#   In this challenge, you will print out the area of a right-angled triangle 
+#   when given its width and height which are given as two command line arguments
+#
+#   The formula for the area of a right angled triangle is:
+#   0.5 * width * height
+#   
+#   Erroneous cases should be handled (wrong input type, negative integers, etc)
+#
+#   Example test cases:
+#
+#   py challenge1.py 7 2 -> 7
+#   py challenge1.py 5 3 -> 7.5
+#   py challenge1.py 0 0 -> 0
+#   py challenge1.py 2 -> Error
+#   py challenge1.py "hello" -> Error
+#   py challenge1.py -1 0 -> Error
+#
+#   Bonus Marks: 
+#
+#   Print out some units after the area on the same line
+#    
+#   Example test case:
+#
+#   py challenge1.py 3 5 -> 7.5 cm^2
+
+CHALLENGE = 3
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    print("Challenge %d" % CHALLENGE)
+    print()
+    main()
---
+++ challenges/answers/challenge4.py
@@ -0,0 +1,29 @@
+# Challenge 4
+
+#   Description:
+#   
+#   In this challenge, you should write a function to convert some number of hours
+#   into minutes and seconds. The hours to be converted will be passed as a
+#   command line argument, and erroneous cases should be handled correctly.
+#   
+#   Note briefly: it is not a problem if your function prints 0 for either minutes or seconds
+#   if the hours divides into them exactly. It also doesn't matter whether they are printed
+#   on the same or different lines.
+#
+#   Example test cases:
+#
+#   py challenge1.py 3 -> 180 minutes and 0 seconds
+#   py challenge1.py 7.5 -> 420 minutes 30 minutes
+#   py challenge1.py 0 -> 0 minutes and 0 seconds
+#   py challenge1.py -1 -> Error (as we can't go back in time)
+#   py challenge1.py "hell9" -> Error
+
+CHALLENGE = 4
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    print("Challenge %d" % CHALLENGE)
+    print()
+    main()
